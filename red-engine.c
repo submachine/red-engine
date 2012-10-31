55ab193e60d5138e2abe9963118ad9afd2e7b3d5
Arjun Shankar <arjun.is@lostca.se>
2012-10-31 16:23:30 +0100

    DB queries for request handling; more canned responses.

--- red-engine.c
+++ red-engine.c
@@ -1,5 +1,6 @@
 #include "red-engine.h"
 
+#include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 #include <db.h>
@@ -22,11 +23,46 @@ static DB *db;
 and HEAD.  */
 #define BODY_MNA "<html><head><title>405 Method Not Allowed</title>" \
                  "</head><body>Method Not Allowed</body></html>"
-
 static struct MHD_Response *response_MNA = NULL;
 
-static int init_mhd_daemon (const struct red_conf conf);
-static int init_db (const struct red_conf conf);
+/* An HTTP 404 - 'Not Found' response. For non-existent redirect URLs.  */
+#define BODY_NF "<html><head><title>404 Not Found</title>" \
+                "</head><body>Not Found</body></html>"
+static struct MHD_Response *response_NF = NULL;
+
+/* An HTTP 500 - 'Internal Server Error' response.  */
+#define BODY_ISE "<html><head><title>500 Internal Server Error</title>" \
+                 "</head><body>Internal Server Error</body></html>"
+static struct MHD_Response *response_ISE = NULL;
+
+static struct MHD_Response *response_empty = NULL;
+
+/* Static helper declarations.  */
+static int
+init_mhd_daemon (const struct red_conf conf);
+
+static int
+init_db (const struct red_conf conf);
+
+static int
+red_handler (void *cls,
+             struct MHD_Connection *connection,
+             const char *url,
+             const char *method,
+             const char *version,
+             const char *upload_data,
+             size_t *upload_data_size,
+             void **con_cls);
+
+static int
+queue_response_for_request (struct MHD_Connection *connection,
+                            const char *url,
+                            const char *method);
+
+
+/* This page contains function definitions for declarations exposed in the
+engine header.  */
+
 
 /* Initializes the redirect engine. Returns 0 on success, -1 on error. */
 int
@@ -39,6 +75,21 @@ red_init (const struct red_conf conf)
   response_MNA = MHD_create_response_from_buffer (strlen (BODY_MNA),
                                                   BODY_MNA,
                                                   MHD_RESPMEM_PERSISTENT);
+
+  /* Prep a canned 404 'Not Found' response.  */
+  response_NF = MHD_create_response_from_buffer (strlen (BODY_NF),
+                                                 BODY_NF,
+                                                 MHD_RESPMEM_PERSISTENT);
+
+  /* Prep a canned 500 'Internal Server Error' response.  */
+  response_ISE = MHD_create_response_from_buffer (strlen (BODY_ISE),
+                                                  BODY_ISE,
+                                                  MHD_RESPMEM_PERSISTENT);
+
+  /* Prep a canned empty response.  */
+  response_empty = MHD_create_response_from_buffer (strlen (""),
+                                                    "",
+                                                    MHD_RESPMEM_PERSISTENT);
 
   if (init_mhd_daemon (conf) < 0)
     LOG_AND_RET ("Unable to start microhttpd daemon.", -1);
@@ -56,6 +107,9 @@ red_terminate (void)
 
   /* Destroy canned responses.  */
   MHD_destroy_response (response_MNA);
+  MHD_destroy_response (response_NF);
+  MHD_destroy_response (response_ISE);
+  MHD_destroy_response (response_empty);
 
   /* Close the Berkeley DB environment.  */
 
@@ -67,6 +121,8 @@ red_terminate (void)
 
   return 0;
 }
+
+/* This page contains static helper functions.  */
 
 
 /* An MHD_AccessHandlerCallback for the redirect engine.
@@ -81,7 +137,6 @@ red_handler (void *cls,
              size_t *upload_data_size,
              void **con_cls)
 {
-  struct MHD_Response *response;
   int ret;
 
   /* We only support GET and HEAD requests.  */
@@ -91,13 +146,15 @@ red_handler (void *cls,
       ret = MHD_queue_response (connection,
                                 MHD_HTTP_METHOD_NOT_ALLOWED,
                                 response_MNA);
-      return ret;
+    }
+  else
+    {
+      ret = queue_response_for_request (connection, url, method);
     }
 
-  /* TODO: This is just a placeholder which returns a sane response.  */
-  response = MHD_create_response_from_buffer (0, NULL, MHD_RESPMEM_PERSISTENT);
-  ret = MHD_queue_response (connection, MHD_HTTP_SERVICE_UNAVAILABLE, response);
-  MHD_destroy_response (response);
+  if (ret == MHD_NO)
+    syslog (LOG_WARNING, _("Failed to respond to '%s %s"), method, url);
+
   return ret;
 }
 
@@ -162,4 +219,60 @@ init_mhd_daemon (const struct red_conf conf)
 
   syslog (LOG_NOTICE, _("Started listening on port %d.\n"), conf.port);
   return 0;
+}
+
+
+static int
+queue_response_for_request (struct MHD_Connection *connection,
+                            const char *url,
+                            const char *method)
+{
+  DBT key, value;
+  unsigned int status_code;
+  struct MHD_Response *response;
+
+  memset (&key, 0, sizeof (DBT));
+  memset (&value, 0, sizeof (DBT));
+
+  key.data = strdup (url);
+  key.size = key.ulen = strlen (url) + 1;
+  key.flags = DB_DBT_USERMEM;
+
+  value.data = NULL;
+  value.size = value.ulen = 0;
+  value.flags = DB_DBT_MALLOC;
+
+  /* If this is a HEAD request, we will send an empty response no matter
+   * what.  */
+  response = response_empty;
+
+  switch (db->get (db, NULL, &key, &value, 0))
+    {
+      case 0:
+        /* Redirect location was found.  */
+        /* TODO: Implement this.  */
+        status_code = MHD_HTTP_INTERNAL_SERVER_ERROR;
+        if (strcmp (method, MHD_HTTP_METHOD_HEAD))
+          response = response_ISE;
+        break;
+
+      case DB_NOTFOUND:
+        /* Redirect location was not found.  */
+        status_code = MHD_HTTP_NOT_FOUND;
+        if (strcmp (method, MHD_HTTP_METHOD_HEAD))
+          response = response_NF;
+        break;
+
+      default:
+        /* Something went wrong while accessing the DB.  */
+        status_code = MHD_HTTP_INTERNAL_SERVER_ERROR;
+        if (strcmp (method, MHD_HTTP_METHOD_HEAD))
+          response = response_ISE;
+        break;
+    }
+
+  free (key.data);
+  free (value.data);
+
+  return MHD_queue_response (connection, status_code, response);
 }
